6bbd62ee9bf8826731ca2fc3a3e5cab1b527cfc2
Anka Kozłowska <kozlowskanka@gmail.com>
2019-10-12 12:49:04 +0200

    Showing proper article

--- js/script.js
+++ js/script.js
@@ -33,8 +33,48 @@ const titleClickHandler = function(event){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post';
+const optTitleSelector = '.post-title';
+const optTitleListSelector = '.titles';
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+function generateTitleLinks(){
+  console.log('generate:', generateTitleLinks)
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* find all the articles and save them to variable:articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+  /* for each article */
+  for(let article of articles){
+    console.log('for each:', article);
+ 
+  /* get the article id */ /*get atrribute ale z czego?? */
+    const articleId = article.getAttribute('id');
+    console.log('id:', articleId);
+
+  /* find the title element and get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+  /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+``
+  /* insert link into html variable */
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 }
+
+generateTitleLinks();
